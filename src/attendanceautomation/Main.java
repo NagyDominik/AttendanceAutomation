145beb5dc3a7660f8ac5b8f4c762a3e051263045
Péter Sebők <sebok.peter97@gmail.com>
2018-02-02 11:58:28 +0100

    Fixes

--- src/attendanceautomation/Main.java
+++ src/attendanceautomation/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../GUI/View/MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("GUI/View/MainWindow.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
