8a1926b57a06384a40c719b04fa2193992d0326a
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-02-05 12:58:36 +0100

    made controller for main

--- src/attendanceautomation/GUI/Controller/MainController.java
+++ src/attendanceautomation/GUI/Controller/MainController.java
@@ -1,4 +1,21 @@
 package attendanceautomation.GUI.Controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    private JFXTextField usernameField;
+    @FXML
+    private JFXTextField passwordField;
+
+    @FXML
+    private void loginClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void helpClicked(ActionEvent event) {
+    }
 }
